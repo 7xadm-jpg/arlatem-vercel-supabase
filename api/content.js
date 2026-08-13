d43a99173c9e3cd4145a1b56c8277813e9830c31
7xadm-jpg <7xadm@7xcapybara.com.br>
2026-08-12 21:10:20 -0300

    Update content.js

--- api/content.js
+++ api/content.js
@@ -1,12 +1,26 @@
 const { sendJson } = require('../lib/http');
-const { readStoreSafe } = require('../lib/store');
+const { getDefaultPayload } = require('../lib/default-data');
 
 module.exports = async (req, res) => {
-  if (req.method !== 'GET') return sendJson(res, 405, { ok: false, message: 'Método não permitido' });
+  if (req.method !== 'GET') {
+    return sendJson(res, 405, { ok: false, message: 'Método não permitido' });
+  }
+
   try {
-    const data = await readStoreSafe();
-    return sendJson(res, 200, { ok: true, data });
+    const data = getDefaultPayload();
+
+    return sendJson(
+      res,
+      200,
+      { ok: true, data },
+      {
+        'Cache-Control': 'public, max-age=0, s-maxage=300, stale-while-revalidate=86400'
+      }
+    );
   } catch (error) {
-    return sendJson(res, 500, { ok: false, message: error.message || 'Falha ao carregar conteúdo' });
+    return sendJson(res, 500, {
+      ok: false,
+      message: error.message || 'Falha ao carregar conteúdo'
+    });
   }
 };
